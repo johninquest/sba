8840b271264ed2725ae481bf6033f6e84d020dd8
John <john-francis.edah@tech11.com>
2023-12-12 15:16:27 +0100

    fixed upgrade to ng17 issue

--- src/app/pages/default/default.component.ts
+++ src/app/pages/default/default.component.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class DefaultComponent {
 
-  now = new Date()
+/*   now = new Date() */
+
+  ngOnInit(): void {
+    this.updateDateTime();
+  }
+
+  currentDateTime!: Date;
+
+  updateDateTime() {
+    setInterval(() => {
+      this.currentDateTime = new Date();
+    }, 1000);
+    /* Update every 1 second (1000 milliseconds) */
+  }
 
 }
